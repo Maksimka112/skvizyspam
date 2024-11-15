fef97d9e30d19acfaac88ec058c5784e8385a04b
Maksimka112 <maks.lyubimov.2007@bk.ru>
2024-11-16 02:21:13 +0500

    skvizyboy

---
+++ Core/TBanner.py
@@ -0,0 +1,13 @@
+﻿from colorama import Fore
+
+
+
+_banner = '''
+SKVIZY_SPAM = Bomber - SMS, CALL
+MIX - together sms & call'''
+
+
+
+def banner(host, port):
+    print(Fore.RED + _banner)
+    print(f'http://{host}:{port}')
